cd9d3dcb10b43dad4cdbb7a7e21213b27b00d3d1
aksharan2022-maker <aksharan2022@gmail.com>
2026-09-15 19:23:41 -0700

    Add files via upload

---
+++ sample 2.py
@@ -0,0 +1,15 @@
+num1=45
+print(num1)
+print(type(num1))
+
+num2=45.67
+print(num2)
+print (type(num2))
+
+name="Aksharan"
+print(name)
+print(type(name))
+
+result=True
+print(result)
+print(type(result))
